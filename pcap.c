da5ba71d4d42e64aa9c5ea62b431a768def7bf26
yeekrim <kr031106@naver.com>
2024-04-02 22:07:36 +0900

    2

--- pcap.c
+++ pcap.c
@@ -2,7 +2,7 @@
 #include <pcap.h>
 #include "myheader.h"
 
-void Info_Packet() {
+void Info_Packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
     printf("Ethernet Header : src mac - %s / dst mac - %s", ethheader.ether_shost,ethheader.ether_dhost);
     printf("IP Header : src ip - %s / dst ip - %s", ipheader.iph_sourceip,ipheader.iph_destip);
     printf("TCP Header : src ip - %s / dst ip - %s", tcpheader.iph_sourceip,ipheader.iph_destip);
@@ -12,25 +12,25 @@ void Info_Packet() {
 
 int main() {
     pcap_t *handle;
-  char errbuf[PCAP_ERRBUF_SIZE];
-  struct bpf_program fp;
-  char filter_exp[] = "icmp";
-  bpf_u_int32 net;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    struct bpf_program fp;
+    char filter_exp[] = "icmp";
+    bpf_u_int32 net;
 
-  // Step 1: Open live pcap session on NIC with name enp0s3
-  handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, errbuf); 
+    // Step 1: Open live pcap session on NIC with name enp0s3
+    handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, errbuf); 
 
 
-  // Step 2: Compile filter_exp into BPF psuedo-code
-  pcap_compile(handle, &fp, filter_exp, 0, net);              
-  if (pcap_setfilter(handle, &fp) !=0) {
+    // Step 2: Compile filter_exp into BPF psuedo-code
+    pcap_compile(handle, &fp, filter_exp, 0, net);              
+    if (pcap_setfilter(handle, &fp) !=0) {
       pcap_perror(handle, "Error:");
       exit(EXIT_FAILURE);
-  }
+    }
 
-  // Step 3: Capture packets
-  pcap_loop(handle, -1, got_packet, NULL);                    
+    // Step 3: Capture packets
+    pcap_loop(handle, -1, Info_Packet, NULL);                    
 
-  pcap_close(handle);   //Close the handle
-  return 0;
+    pcap_close(handle);   //Close the handle
+    return 0;
 }
